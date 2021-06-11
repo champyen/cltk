f78286633f8c23f712c237d34daadf66fea36995
Champ Yen <champ.yen@gmail.com>
2021-06-12 01:31:56 +0800

    add final scene example

--- RayTracing/InOneWeekend/final.cpp
+++ RayTracing/InOneWeekend/final.cpp
@@ -8,7 +8,7 @@
 #include "bmp_fmt.h"
 #include "vec3.h"
 
-#define MAX_OBJS    256
+#define MAX_OBJS    2048
 
 #include "material.h"
 
@@ -85,33 +85,61 @@ typedef struct camera
 
 void camera_example(void)
 {
+    srand(time(NULL));
     // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    auto aspect_ratio = 3.0 / 2.0;
+    int image_width = 600;
     int image_height = static_cast<int>(image_width / aspect_ratio);;
 
     cltk::context ctx;
     shared_ptr<cltk::buffer> buf = ctx.allocBuffer(image_width*image_width*3);
     shared_ptr<cltk::buffer> sphere_list = ctx.allocBuffer(sizeof(sphere)*MAX_OBJS);
-    shared_ptr<cltk::library> lib = ctx.getLibrary("camera2.cl", "", "");
-    shared_ptr<cltk::function> func = lib->getFunction("camera2");
+    shared_ptr<cltk::library> lib = ctx.getLibrary("final.cl", "", "");
+    shared_ptr<cltk::function> func = lib->getFunction("final");
 
     // Camera
-    point3 lookfrom(3,3,2);
-    point3 lookat(0,0,-1);
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
     vec3 vup(0,1,0);
-    auto dist_to_focus = (lookfrom-lookat).length();
-    auto aperture = 2.0;
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
 
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // World
     int num_sphere = 0;
     sphere *splist = (sphere *)sphere_list->map();
-    splist[num_sphere++] = sphere(cl_float3{0,-100.5,-1}, 100.f, cl_float3{0.8, 0.8, 0.0}, RT_M_LAMBERTIAN, 0);
-    splist[num_sphere++] = sphere(cl_float3{ 0.0, 0.0,-1.0}, 0.5f, cl_float3{0.3, 0.3, 0.7}, RT_M_LAMBERTIAN, 0.0);
-    splist[num_sphere++] = sphere(cl_float3{-1.0, 0.0,-1.0}, 0.5f, cl_float3{1.0, 1.0, 1.0}, RT_M_DIELECTRIC, 1.5);
-    splist[num_sphere++] = sphere(cl_float3{ 1.0, 0.0,-1.0}, 0.5f, cl_float3{0.8, 0.6, 0.2}, RT_M_METAL, 1.0);
+    splist[num_sphere++] = sphere(cl_float3{0,-1000,0}, 1000.f, cl_float3{0.5, 0.5, 0.5}, RT_M_LAMBERTIAN, 0);
+    for (int a = -7; a < 7; a++) {
+        for (int b = -7; b < 7; b++) {
+            auto choose_mat = random_float();
+            point3 center(a + 0.9*random_float(), 0.2, b + 0.9*random_float());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+
+                if (choose_mat < 0.4) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    splist[num_sphere++] = sphere(center.e, 0.2, albedo.e, RT_M_LAMBERTIAN, 0);
+                } else if (choose_mat < 0.75) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_float(0, 0.5);
+                    splist[num_sphere++] = sphere(center.e, 0.2, albedo.e, RT_M_METAL, fuzz);
+                } else {
+                    // glass
+                    auto albedo = color(1, 1, 1);
+                    splist[num_sphere++] = sphere(center.e, 0.2, albedo.e, RT_M_DIELECTRIC, 1.5);
+                }
+            }
+        }
+    }
+
+
+    splist[num_sphere++] = sphere(cl_float3{0, 1, 0}, 1.0, cl_float3{1, 1, 1}, RT_M_DIELECTRIC, 1.5);
+    splist[num_sphere++] = sphere(cl_float3{-4, 1, 0}, 1.0, cl_float3{0.4, 0.2, 0.1}, RT_M_LAMBERTIAN, 0.f);
+    splist[num_sphere++] = sphere(cl_float3{4, 1, 0}, 1.0, cl_float3{0.7, 0.6, 0.5}, RT_M_METAL, 0.f);
+
     sphere_list->unmap();
 
     srandom(time(NULL));
@@ -125,7 +153,7 @@ void camera_example(void)
         int* hostbuf = (int*)buf->map();
 
         bmp_header *header = gen_bmp_header(image_width, image_height);
-        FILE *fptr = fopen("camera2.bmp", "wb");
+        FILE *fptr = fopen("final.bmp", "wb");
         fwrite(header, sizeof(bmp_header), 1, fptr);
         fwrite(hostbuf, 3, image_width*image_height, fptr);
         fclose(fptr);
